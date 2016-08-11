f6b6c986dcef3cb65d59ee265d1b017f34f3f62c
Steven Tsang <tsangste@users.noreply.github.com>
2016-08-11 14:19:16 +0100

    Add coding style tools to project (#1)
    * Add .editorconfig file
    
    * Add eslint and update some calls to es6 arrow function
    
    * Add hound config file

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,8 +14,11 @@ app.use(cookieParser());
 
 app.use('/', routes);
 
+// stop favicon error requests
+app.get('/favicon.ico', (req, res) => res.send(200));
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -27,7 +29,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use((err, req, res) => {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -38,7 +40,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
