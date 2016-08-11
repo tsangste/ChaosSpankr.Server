82f08e4647bc9df4c15a5cf33a06a62d0d15f40f
Steven Tsang <tsangste@gmail.com>
2016-08-11 15:35:34 +0100

    Remove all semicolons

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ var routes = require('./routes/index')
 
 var app = express()
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
 
-app.use('/', routes);
+app.use('/', routes)
 
 // stop favicon error requests
-app.get('/favicon.ico', (req, res) => res.send(200));
+app.get('/favicon.ico', (req, res) => res.send(200))
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
 
 // error handlers
 
@@ -30,22 +30,22 @@ app.use((req, res, next) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use((err, req, res) => {
-        res.status(err.status || 500);
+        res.status(err.status || 500)
         res.render('error', {
             message: err.message,
             error: err
-        });
-    });
+        })
+    })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
         message: err.message,
         error: {}
-    });
-});
+    })
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res) => res.json({message: 'Welcome to ChaosSpankr!!!'}));
+router.get('/', (req, res) => res.json({message: 'Welcome to ChaosSpankr!!!'}))
 
-router.get('/test', (req, res) => res.json({message: 'test code'}));
-
-module.exports = router;
+module.exports = router
