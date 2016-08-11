f17edcb39fde866d7e2ea86bb4074b6b14a58161
Steven Tsang <tsangste@users.noreply.github.com>
2016-08-11 15:57:52 +0100

    Update .eslintrc to always error when there is a semicolon (#2)
    * Update .eslintrc to always error when there is a semicolon
    
    * Rename .eslintrc file to include .json extension
    
    * Update .hound config
    
    * Remove all semicolons
    
    * Disable no-console rule and removed semicolon from www file

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var routes = require('./routes/index');
+var routes = require('./routes/index')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
 
-app.use('/', routes);
+app.use('/', routes)
 
 // stop favicon error requests
-app.get('/favicon.ico', (req, res) => res.send(200));
+app.get('/favicon.ico', (req, res) => res.send(200))
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
 
 // error handlers
 
@@ -30,22 +30,22 @@ app.use((req, res, next) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use((err, req, res) => {
-        res.status(err.status || 500);
+        res.status(err.status || 500)
         res.render('error', {
             message: err.message,
             error: err
-        });
-    });
+        })
+    })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
         message: err.message,
         error: {}
-    });
-});
+    })
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res) => res.json({message: 'Welcome to ChaosSpankr!!!'}));
+router.get('/', (req, res) => res.json({message: 'Welcome to ChaosSpankr!!!'}))
 
-module.exports = router;
+module.exports = router
