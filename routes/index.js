ac85ecaba4b87cd51ef6839dfe3c48c9b8931975
Steven Tsang <tsangste@gmail.com>
2016-08-11 15:20:51 +0100

    Add some test code

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => res.json({message: 'Welcome to ChaosSpankr!!!'}));
 
+router.get('/test', (req, res) => res.json({message: 'test code'}));
+
 module.exports = router;
